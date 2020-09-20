4abf14f12656f93c0edb5de0ddf8a592427c6f56
sungmin-choi <53036141+sungmin-choi@users.noreply.github.com>
2020-09-20 17:43:09 +0800

    day3

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+
 const app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "Logout" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "UserDetail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "EditProfile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "ChangePassword" });
