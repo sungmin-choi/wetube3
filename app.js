da4473ba1b417b8035fb0b5993ef9e8fdeeb95b6
sungmin-choi <53036141+sungmin-choi@users.noreply.github.com>
2020-10-05 18:48:37 +0800

    week4 1

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ import globalRouter from "./routers/globalRouter";
 const app = express();
 
 app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(helmet());
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,7 +3,8 @@ import routes from "../routes";
 import { uploadVideo } from "../middlewares";
 import {
   videoDetail,
-  editVideo,
+  getEditVideo,
+  postEditVideo,
   deleteVideo,
   getUpload,
   postUpload,
@@ -12,6 +13,7 @@ const videoRouter = express.Router();
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 export default videoRouter;
